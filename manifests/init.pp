c1ae6b24db73416fd9e03b032440f3809c0dac21
Celso Silva <silvac1@student.lasalle.edu>
2014-09-30 17:23:40 -0400

    my updated init.pp file

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,121 +1,78 @@
 # == Class: osticket
 #
-# This puppet module installs the opensource ticketing system osTicket.
-# Additional information about osTicket can be found here.
-# This software is provided asis so use at your own risk.
+# Full description of class osticket here.
+#
+# === Parameters
+#
+# Document parameters here.
+#
+# [*sample_parameter*]
+#   Explanation of what this parameter affects and what it defaults to.
+#   e.g. "Specify one or more upstream ntp servers as an array."
 #
 # === Variables
 #
-# Currently this Class consumes the following variables.
+# Here you should define a list of variables that this module would require.
 #
-# [*ost_dir*]
-#   The directory which contains the source code for osTicket
-# [*ost_install_dir*]
-#   The directory where osTicket will be installed into
-# [*ost_db_user*]
-#  The username used for the osTicket database connections
-# [*ost_db_password*]
-#   The password used to authenticate the ost_db_user to the osTicket database
-# [*ost_db_name*]
-#   The name of the database osTicket will connect to.
-# [*ost_db_host*]
-#   The host running the osTicket database instance
+# [*sample_variable*]
+#   Explanation of how this variable affects the funtion of this class and if
+#   it has a default. e.g. "The parameter enc_ntp_servers must be set by the
+#   External Node Classifier as a comma separated list of hostnames." (Note,
+#   global variables should be avoided in favor of class parameters as
+#   of Puppet 2.6.)
 #
 # === Examples
 #
-#  class { osticket: }
+#  class { osticket:
+#    servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
+#  }
 #
 # === Authors
 #
-# Author Name <peter@pouliot.net>
+# Author Name <author@domain.com>
 #
 # === Copyright
 #
-# Copyright 2014 Peter J. Pouliot <peter@pouliot.net>, unless otherwise noted.
+# Copyright 2014 Your name here, unless otherwise noted.
 #
-class osticket (
-  $ost_dir   = $osticket::params::ost_dir,
-  $ost_install_dir = $osticket::params::ost_install_dir,
-  $ost_db_name     = $osticket::params::ost_db_name,
-  $ost_db_user     = $osticket::params::ost_db_user,
-  $ost_db_passwd   = $osticket::params::ost_db_passwd,
-  $ost_db_host     = $osticket::params::ost_db_host,
-  $ost_src_url     = $osticket::params::ost_src_url,
-  $osticket_admin  = $osticket::params::ost_admin_email
-) inherits params {
-
-  php::module{['imap','gd',]:
-    notify => [Service['apache2'],Exec['enable-php5-imap']],
-  }
-  exec {'enable-php5-imap':
-    command     => '/usr/sbin/php5enmod imap',
-    unless      => '/usr/sbin/php5query -M |grep imap',
-    refreshonly => true,
-  }
-
+class osticket {
   class{'apache':
-    default_vhost  => true,
-    mpm_module     => prefork,
+    default_vhost => true,
+    mpm_module => prefork,
     service_enable => true,
     service_ensure => running,
   }
   class {'apache::mod::php':}
 
-  apache::vhost {'osTicket':
-    priority   => '10',
+  apache::vhost {'osticket':
+    priority => '10',
     vhost_name => $::ipaddress,
-    port       => 80,
-    docroot    => $ost_install_dir,
-    logroot    => "/var/log/${module_name}",
-    require    => Vcsrepo[$ost_dir],
+    port => 80,
+    docroot => '/var/www/html/osticket',
+    logroot => '/var/log/osticket',
+    require => Vcsrepo['/var/www/html/osticket'],
+  }
+  class {'mysql::server':
+    root_password => 'ubuntu',
   }
 
-  class { 'mysql::server':
-    config_hash => { 'root_password' => $ost_db_passwd }
+  mysql::db { 'osticket':
+    user      => 'ubuntu',
+    password  => 'ubuntu',
+    host      => 'localhost',
+    grant     => ['CREATE','INSERT','SELECT','DELETE','UPDATE'],
+    require   => Class['mysql::server],
+  }
+  vcsrepo { '/var/www/html/osticket/':
+    ensure    => present,
+    provider  => git,
+    source    => 'https://github.com/osTicket/osTicket-1.8/'
   }
 
-  mysql::db { $ost_db_name:
-    user     => $ost_db_user,
-    password => $ost_db_passwd,
-    host     => $ost_db_host,
-    grant    => ['all'],
+  file {'/var/www/html/osticket/include/ost-config':
+    ensure   => file,
+    source   => '/var/www/html/osticket/include/ost-config.sample.php',
+    mode     => '0644',
+    require  => Vcsrepo['/var/www/html/osticket'],
   }
-
-  vcsrepo { $ost_dir:
-    ensure   => present,
-    provider => git,
-    source   => $ost_src_url,
-    require  => Package['php5-gd'],
-    owner    => 'www-data',
-    group    => 'www-data',
-    notify   => Exec['Run-OsTicket-Setup'],
-  }
-
-#  file {"${ost_dir}/include/ost-config.php":
-  file {"${ost_install_dir}/include/ost-config.php":
-    ensure  => file,
-    content => template("${module_name}/ost-config.php.erb"),
-    mode    => '0655',
-    require => Apache::Vhost['osTicket'],
-#    notify  => Exec['Run-OsTicket-Setup'],
-  }
-
-  exec {'Run-OsTicket-Setup':
-#    command     => "/usr/bin/php5 -q ./install.php",
-    command     => "/usr/bin/php5 setup/cli/manage.php deploy --setup ${ost_install_dir}",
-    cwd         => $ost_dir,
-    refreshonly => true,
-#    require     => File["${ost_dir}/include/ost-sampleconfig.php"],
-    require     => [ Apache::Vhost['osTicket'], Vcsrepo[$ost_dir]],
-    logoutput   => true,
-  }
-
-#  file {"${ost_install_dir}/include/ost-sampleconfig.php":
-#    ensure  => absent,
-#    require => File["${ost_install_dir}/include/ost-config.php"],
-#  }
-#  file {"${ost_install_dir}/setup":
-#    ensure  => absent,
-#    require => File["${ost_install_dir}/include/ost-config.php"],
-#  }
 }
